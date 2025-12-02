a81c9fb30c3141dc0d5302cb675f5391aaf2eb7b
Christian Ebeling <Christian.Ebeling@scai.fraunhofer.de>
2025-12-02 18:05:21 +0100

    new constants used in other modules

--- src/biokb_wcvp/constants.py
+++ src/biokb_wcvp/constants.py
@@ -10,6 +10,7 @@ PROJECT_NAME = "wcvp"
 PROJECT_FOLDER = os.path.join(BIOKB_FOLDER, PROJECT_NAME)
 DATA_FOLDER = os.path.join(PROJECT_FOLDER, "data")
 LOGS_FOLDER = os.path.join(DATA_FOLDER, "logs")  # where to store log files
+ZIPPED_TTLS_PATH = os.path.join(DATA_FOLDER, "ttls.zip")
 
 DOWNLOAD_URL = "https://sftp.kew.org/pub/data-repositories/WCVP/wcvp.zip"
 PATH_TO_ZIP_FILE = os.path.join(DATA_FOLDER, "wcvp.zip")
@@ -17,6 +18,11 @@ DEFAULT_PATH_UNZIPPED_DATA_FOLDER = os.path.join(DATA_FOLDER, "unzipped")
 DB_DEFAULT_CONNECTION_STR = "sqlite:///" + os.path.join(BIOKB_FOLDER, "biokb.db")
 
 
-class TsvFileName(StrEnum):
-    DISTRIBUTION = "wcvp_distribution.csv"
-    NAMES = "wcvp_names.csv"
+DISTRIBUTION_FILE = "wcvp_distribution.csv"
+NAMES_FILE = "wcvp_names.csv"
+
+TAXONOMY_URL = "https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdmp.zip"
+TAXONOMY_DATA_FOLDER = os.path.join(BIOKB_FOLDER, "taxtree", "data")
+
+BASIC_NODE_LABEL = "DbWCVP"
+EXPORT_FOLDER = os.path.join(DATA_FOLDER, "ttls")
